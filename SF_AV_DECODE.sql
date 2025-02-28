2174a175dcb41541f8604e526fa8709a309309a5
chiragdave2001 <76464792+chiragdave2001@users.noreply.github.com>
2025-02-28 10:44:09 +0530

    Add files via upload

---
+++ SF_AV_DECODE.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION SF_AV_DECODE("IN_AV_ID" NUMBER(35,4))
+RETURNS STRING
+LANGUAGE SQL
+
+AS '
+  /*********************************************************************
+  * Name   SF_ALLOWABLE_VALUES
+  * Purpose   Decodes av id and returns description
+  *    if av id is found else return null
+  * Input Parameter  Code_in   Varchar2
+  * Output Parameter Code_desc Varchar2
+  * Edit History
+  *
+  *  Name    Date           Comment
+  *
+  *  CMS     01/21/1997     Date created
+  *********************************************************************
+  */
+    SELECT MAX(VALUE_SHORT_DESC)
+    FROM T_ALLOWABLE_VALUES
+    WHERE AV_ID = IN_AV_ID
+';
